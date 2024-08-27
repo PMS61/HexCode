3c70d2667333f463c29b00b836c0a3e69f8db61a
aitwehrrg <rrginfinity@gmail.com>
2024-08-27 13:16:25 +0530

    Added props to map

--- hex-frontend/src/components/Dashboard/Map.jsx
+++ hex-frontend/src/components/Dashboard/Map.jsx
@@ -3,10 +3,10 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'font-awesome/css/font-awesome.min.css'; // Include FontAwesome CSS
 
-const Map = () => {
+const Map = ({ latitude, longitude, zoomLevel = 12, locationName = "Mumbai", width = '1000px', height = '400px' }) => {
     useEffect(() => {
         // Initialize the map
-        const map = L.map('map').setView([19.0760, 72.8777], 12);
+        const map = L.map('map').setView([latitude, longitude], zoomLevel);
 
         L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=TjqLUOrWKEBi18GTwW6V', {
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
@@ -21,11 +21,22 @@ const Map = () => {
             popupAnchor: [0, -24]
         });
 
-        const marker = L.marker([19.0760, 72.8777], { icon: fontAwesomeIcon }).addTo(map);
-        marker.bindPopup("<b>Mumbai</b><br> Maharashtra, India.").openPopup();
-    }, []);
+        const marker = L.marker([latitude, longitude], { icon: fontAwesomeIcon }).addTo(map);
+        marker.bindPopup(`<b>${locationName}</b><br> Latitude: ${latitude}, Longitude: ${longitude}`).openPopup();
+    }, [latitude, longitude, zoomLevel, locationName]);
 
-    return <div id="map" style={{ width: '600px', height: '400px', margin: '0 auto' }}></div>;
+    return <div id="map" style={{ width: width, height: height, margin: '0 auto' }}></div>;
 };
 
 export default Map;
+
+/*
+Use the map as:
+    <Map 
+        latitude={latitude} 
+        longitude={longitude} 
+        locationName={locationName} 
+        width={width} 
+        height={height} 
+    />
+*/
