b0b494aff4bbfb1855a8f99d9cbeeac377a7d996
Tanish2207 <bhamare.tn@gmail.com>
2024-08-31 15:47:13 +0530

    feat: Add features section in landing pg

--- hex-frontend/src/components/LandingNew/LandingNew.jsx
+++ hex-frontend/src/components/LandingNew/LandingNew.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { Link } from "react-router-dom";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const LandingNew = () => {
+  const data = {
+    labels: ["Money actually used for Road repairs", "Money wasted"],
+    datasets: [
+      {
+        data: [900, 350],
+        backgroundColor: ["#E24D42", "#EAB839"],
+      },
+    ],
+    hoverOffset: 4,
+  };
+  const options = {};
+
   return (
     <>
       <section id="hero">
@@ -44,10 +60,8 @@ export const LandingNew = () => {
         <div className="mb-10 h-1 w-40 bg-red-400 border"></div>
       </section>
 
-      <section
-        className="flex flex-col justify-center items-center"
-      >
-        <div className="mt-20 text-center w-full flex flex-col gap-4">
+      <section className="my-16 flex flex-col justify-center items-center">
+        <div className=" text-center w-full flex flex-col gap-4 justify-center items-center">
           <h1
             className="font-medium text-3xl"
             style={{ fontFamily: "Poppins" }}
@@ -55,44 +69,211 @@ export const LandingNew = () => {
             Need of UrbanConnect
           </h1>
 
-          <div className="mt-4 flex w-full">
-            <div className="w-1/3 h-72 p-8 flex flex-col gap-10" style={{ backgroundColor: "#B5C2FF" }}>
-            <div style={{height: "25%"}}>
-              <h1 className="font-bold" style={{fontSize: "1.35rem", fontFamily: "Poppins"}}>Underutilization of Resources</h1>
+          <div className="mt-4 flex w-3/4 gap-10">
+            <div
+              className="w-1/3 h-96 p-8 flex flex-col gap-10 rounded-lg hover:shadow-xl hover:transition-all"
+              style={{
+                backgroundColor: "#B5C2FF",
+                border: "1px solid #5C5C5C",
+              }}
+            >
+              <div style={{ height: "25%" }}>
+                <h1
+                  className="font-bold"
+                  style={{ fontSize: "1.35rem", fontFamily: "Poppins" }}
+                >
+                  Underutilization of Resources
+                </h1>
               </div>
-              <p className="text-justify" style={{fontFamily: "Poppins"}}>
+              <div className="h-0.5 w-full bg-slate-400 "></div>
+              <p
+                className="text-justify tracking-wide"
+                style={{ fontFamily: "Poppins" }}
+              >
                 Due to the overlapping jurisdictions and responsibilities,
                 resources available with various departments are often
                 underutilized, resulting in wastage and inefficiency
               </p>
             </div>
-            <div className="w-1/3 h-72 p-8 flex flex-col gap-10" style={{ backgroundColor: "#9FFFCB" }}>
-            <div style={{height: "25%"}}>
-              <h1 className="font-bold" style={{fontSize: "1.35rem", fontFamily: "Poppins"}}>Miscoordination and Delays</h1>
+
+            <div
+              className="w-1/3 h-96 p-8 flex flex-col gap-10 rounded-lg hover:shadow-xl hover:transition-all"
+              style={{
+                backgroundColor: "#9FFFCB",
+                border: "1px solid #5C5C5C",
+              }}
+            >
+              <div style={{ height: "25%" }}>
+                <h1
+                  className="font-bold"
+                  style={{ fontSize: "1.35rem", fontFamily: "Poppins" }}
+                >
+                  Miscoordination and Delays
+                </h1>
               </div>
-              <p className="text-justify" style={{fontFamily: "Poppins"}}>
+              <div className="h-0.5 w-full bg-slate-400 "></div>
+              <p
+                className="text-justify tracking-wide"
+                style={{ fontFamily: "Poppins" }}
+              >
                 Execution of multiple projects at the same site by different
                 agencies lead to interference, miscoordination, and delays
               </p>
             </div>
-            <div className="w-1/3 h-72 p-8 flex flex-col gap-10" style={{ backgroundColor: "#B5C2FF" }}>
-            <div style={{height: "25%"}}>
-              <h1 className="font-bold" style={{fontSize: "1.35rem", fontFamily: "Poppins"}}>Multiplicity of Authorities and Implementing Agencies</h1>
+
+            <div
+              className="w-1/3 h-96 p-8 flex flex-col gap-10 rounded-lg hover:shadow-xl hover:transition-all"
+              style={{
+                backgroundColor: "#B5C2FF",
+                border: "1px solid #5C5C5C",
+              }}
+            >
+              <div style={{ height: "25%" }}>
+                <h1
+                  className="font-bold"
+                  style={{ fontSize: "1.20rem", fontFamily: "Poppins" }}
+                >
+                  Multiplicity of Authorities and Implementing Agencies
+                </h1>
               </div>
-              <p className="text-justify" style={{fontFamily: "Poppins"}}>
-              The presence of multiple authorities and implementing agencies in Indian urban areas creates governance challenges, leading to inefficiencies
+              <div className="h-0.5 w-full bg-slate-400 "></div>
+              <p
+                className="text-justify tracking-wide"
+                style={{ fontFamily: "Poppins" }}
+              >
+                The presence of multiple authorities and implementing agencies
+                in Indian urban areas creates governance challenges, leading to
+                inefficiencies
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="py-10 w-full flex justify-around items-center" style={{backgroundColor: "#F2F2F2"}}>
-        <div className="w-1/3"><img src="temp-graph.png" alt="" /></div>
-        <div className="w-1/3 flex flex-col gap-14">
-          <h1 className="text-center font-semibold text-2xl" style={{fontFamily: "Poppins"}}>Statistics</h1>
-          <h2 className="font-base text-justify text-lg" style={{fontFamily: "Poppins"}}>According to estimates, Mumbai's Brihanmumbai Municipal Corporation (BMC) spends around ₹1,000-₹1,500 crores annually on road repairs and maintenance. Of this, approximately 20-30% is believed to be wasted due to rework caused by lack of coordination with other utility departments (about ₹200-₹450 crores).</h2>
+      <section
+        className="py-10 w-full flex justify-around items-center"
+        style={{ backgroundColor: "#F2F2F2" }}
+      >
+        <div className="w-1/3">
+          <div className="w-96">
+            <Pie data={data} options={options}></Pie>
+          </div>
         </div>
+        <div className="w-1/3 flex flex-col gap-14">
+          <h1
+            className="text-center font-semibold text-2xl"
+            style={{ fontFamily: "Poppins" }}
+          >
+            Statistics
+          </h1>
+          <h2
+            className="font-base text-justify text-lg"
+            style={{ fontFamily: "Poppins" }}
+          >
+            According to estimates, Mumbai's{" "}
+            <span className="font-bold">
+              Brihanmumbai Municipal Corporation
+            </span>
+            (BMC) spends around{" "}
+            <span className="font-bold">₹1,000-₹1,500 crores</span> annually on
+            road repairs and maintenance. Of this, approximately 20-30% is
+            believed to be wasted due to rework caused by lack of coordination
+            with other utility departments (about{" "}
+            <span className="font-bold">₹200-₹450 crores</span>).
+          </h2>
+        </div>
+      </section>
+
+      <section className="my-14 flex flex-col justify-center items-center">
+        <h1 className="font-medium text-3xl" style={{ fontFamily: "Poppins" }}>
+          Features
+        </h1>
+        <div
+          className="mt-8 grid grid-cols-12 grid-rows-2 rounded-md"
+          style={{ backgroundColor: "#EBEBEB", width: "85%" }}
+        >
+          <div
+            className="p-5 flex flex-col gap-3 text-center justify-center items-center"
+            style={{ gridColumn: "span 4" }}
+          >
+            <img src="share-icon.svg" alt="" style={{ width: "30%" }} />
+            <p
+              className="text-sm font-normal"
+              style={{ fontFamily: "Poppins" }}
+            >
+              Exchange of data & resources and list of ongoing and upcoming
+              projects between various agencies
+            </p>
+          </div>
+          <div
+            className="p-5 flex flex-col gap-3 text-center justify-center items-center"
+            style={{ gridColumn: "span 4" }}
+          >
+            <img src="calendar-icon.png" alt="" style={{ width: "30%" }} />
+            <p
+              className="text-sm font-normal"
+              style={{ fontFamily: "Poppins" }}
+            >
+              {" "}
+              Scheduling of tasks, reports, and work distribution between
+              agencies for projects categorized as "Inter-departmental"
+            </p>
+          </div>
+          <div
+            className="p-5 flex flex-col gap-3 text-center justify-center items-center"
+            style={{ gridColumn: "span 4" }}
+          >
+            <img src="location-icon.svg" alt="" style={{ width: "30%" }} />
+            <p
+              className="text-sm font-normal"
+              style={{ fontFamily: "Poppins" }}
+            >
+              {" "}
+              Identification of projects in different departments which share
+              the project site, to reduce costs incurred during planning and
+              execution of projects
+            </p>
+          </div>
+          <div
+            className="p-5 flex flex-col gap-3 text-center justify-center items-center"
+            style={{ gridColumn: "3 /span 4" }}
+          >
+            <img src="seminar-icon.png" alt="" style={{ width: "30%" }} />
+            <p
+              className="text-sm font-normal"
+              style={{ fontFamily: "Poppins" }}
+            >
+              Training and capacity building exercises, workshops & seminars
+            </p>
+          </div>
+          <div
+            className="p-5 flex flex-col gap-3 text-center justify-center items-center"
+            style={{ gridColumn: "7 /span 4" }}
+          >
+            <img src="forum-icon.png" alt="" style={{ width: "30%" }} />
+            <p
+              className="text-sm font-normal"
+              style={{ fontFamily: "Poppins" }}
+            >
+              Discussion forum with Intra department, inter department, and
+              public form sections
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <Link to="/signup">
+        <div className="flex justify-center items-center my-4">
+          <button className="w-80 flex text-white bg-slate-900 justify-center items-center gap-3 hover:bg-slate-950">
+            Onboard your Department now <img src="arrow.svg" width="10%" />
+          </button>
+        </div>
+      </Link>
+      <section
+        className="text-center py-2 text-sm text-white w-full bg-slate-800"
+        style={{ fontFamily: "Poppins" }}
+      >
+        © UrbanConnect 2024
       </section>
     </>
   );
